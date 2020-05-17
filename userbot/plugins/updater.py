c9503dd48f0ccc39f99a5f57e294951b42849175
Crackexy <63198834+Crackexy@users.noreply.github.com>
2020-05-17 14:05:21 +0530

    Upadated latest

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -44,7 +44,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://GitHub.com/xditya/TeleBot"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/Crackexy/CrackBot"
 BOT_IS_UP_TO_DATE = "`The userbot is up-to-date.\nThank you for Using this Service.`"
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
@@ -168,7 +168,7 @@ def generate_change_log(git_repo, diff_marker):
 
 async def deploy_start(tgbot, message, refspec, remote):
     await message.edit(RESTARTING_APP)
-    await message.edit("Updating and Deploying New Branch. Please wait for 5 minutes then use `.alive` to check if i'm working or not.")
+    await message.edit("Updating and Deploying To New Branch. 5 minute Ruk and then use `.alive` to check if i'm working or not.")
     await remote.push(refspec=refspec)
     await tgbot.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
